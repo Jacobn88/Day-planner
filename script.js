b040a7005f14dabad1cba2a914fabdd9fc88c07c
Jacob Nelson <NelsonJacob1988@gmail.com>
2020-10-01 21:13:42 -0500

    save state on button click

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// <!-- <div class="row">
-// <div class="col-md-12">
-//   <div class="row">
-//     <div class="col-md-2"></div>
-//     <div class="col-md-8"></div>
-//     <div class="col-md-2"></div>
-//   </div> -->
 const M = moment();
+
+$("#currentDay").text(M.format('MMMM Do YYYY, h:mm:ss a'));
 
 var outMostRow = $("<div>");
 outMostRow.addClass("row");
@@ -29,14 +24,15 @@ for (var i = 0; i < 8; i++) {
     textCol.addClass("col-md-8");
     innerRow.append(textCol);
     var newTextArea = $("<textarea>");
-    if(i+9 > M.hour()){
-       newTextArea.addClass("future"); 
-    }else if(i+9 < M.hour()){
+    if (i + 9 > M.hour()) {
+        newTextArea.addClass("future");
+    } else if (i + 9 < M.hour()) {
         newTextArea.addClass("past");
-    }else{
+    } else {
         newTextArea.addClass("present");
     }
-    
+    newTextArea.attr("id", "textArea-" + i);
+    newTextArea.val(localStorage.getItem("textArea-" + i));
     textCol.append(newTextArea);
 
     var btnCol = $("<div>");
@@ -44,10 +40,15 @@ for (var i = 0; i < 8; i++) {
     innerRow.append(btnCol);
 
     var saveBtn = $('<button/>')
-        .text('Test')
-        .click(function () { alert('hi'); });
+        .text('Save')
+        .attr('id', 'btn-' + i)
+        .click(saveItem);
     saveBtn.addClass("saveBtn");
     btnCol.append(saveBtn);
-
-    
+}
+function saveItem(event) {
+    var element = event.target;
+    var index = element.id.substring(4);
+    var id = "textArea-" + index;
+    localStorage.setItem(id, $("#" + id).val());
 }
